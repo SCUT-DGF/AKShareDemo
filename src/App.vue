804eda8e89413b06956d2e800fe63456d2e6daec
王铭远 <admin@SSC-mingyuan>
2024-07-26 10:22:29 +0800

    merge_front

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <FrontStock></FrontStock>
+</template>
+
+<script>
+
+import FrontStock from './components/FrontStock.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FrontStock
+  }
+}
+</script>
+
+<style>
+</style>
